87e3a0dc2d34af5eb19a02b89317aa056b63aa16
Camron <cawe219@gmail.com>
2023-01-20 19:28:42 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const backardsString = word.split("").reverse().join("");
+  return word === backardsString ? true : false;
 }
 
 /* 
-  Add your pseudocode here
+  Need algorithm that loops through a string and gathers information on each characater in the string.
+
+    iterate over each Char in a string:
+      if word === wordbackwards:
+        return true else false
 */
 
 /*
-  Add written explanation of your solution here
+  isPalindrome iterates through a string and grabs each charater using the builtin split function and 
+  then reverses the string using the built in reverse function. Finally rejoins the string using the join function and returns
+  if the word is a palindrome or not.
 */
 
 // You can run `node index.js` to view these console logs
@@ -18,8 +25,30 @@ if (require.main === module) {
 
   console.log("");
 
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
+
+
